765a1f2d219a3cc92a7dc1af8f7a6a461ec703c9
Jesse <jessephilip@gmail.com>
2017-10-19 09:24:22 -0500

    created functions for converting days and months to string

---
+++ src/constants/dateConverter.ts
@@ -0,0 +1,30 @@
+export function weekdayConverter (weekday: number): string {
+  switch (weekday) {
+    case 0: return 'Sunday';
+    case 1: return 'Monday';
+    case 2: return 'Tuesday';
+    case 3: return 'Wednesday';
+    case 4: return 'Thursday';
+    case 5: return 'Friday';
+    case 6: return 'Saturday';
+    default: return 'Error, improper day given to weekConverter function';
+  }
+}
+
+export function monthConverter (month: number): string {
+  switch (month) {
+    case 0: return 'January';
+    case 1: return 'February';
+    case 2: return 'March';
+    case 3: return 'April';
+    case 4: return 'May';
+    case 5: return 'June';
+    case 6: return 'July';
+    case 7: return 'August';
+    case 8: return 'September';
+    case 9: return 'October';
+    case 10: return 'November';
+    case 11: return 'December';
+    default: return 'Error, improper month given to weekConverter function';
+  }
+}
